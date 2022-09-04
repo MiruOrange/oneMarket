a6e580f7d632cc20d76c155bc7929d3e416cddbe
miruken <mafiahsu0.1@gmail.com>
2022-09-04 12:27:34 +0800

    新增商品軍糧丸、修正以for迴圈帶入商品輪播、新增會員登入選項

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,7 @@ from myapp.models import product
 # Create your views here.
 
 def index(request):
+    index =[0, 1, 2, 3, 4, 5, 6, 7] #給商品圖使用的索引值
     return render(request, 'index.html', locals())
 
 
