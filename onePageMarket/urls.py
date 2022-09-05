1f0c0578c7ec800658a34764c68afab88180fb0d
DennisHsuan <boyhood515@gmail.com>
2022-09-05 21:08:02 +0800

    顧客登入及註冊帳號-初步版本

--- onePageMarket/urls.py
+++ onePageMarket/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index), 
     path('index/', views.index),
+    path('userlogin/',views.userlogin),
+    path('useradd/',views.useradd),
 ]
