6692d0e65a601702b2a0be267d9640397206c9dc
miruken <mafiahsu0.1@gmail.com>
2022-09-07 08:43:23 +0800

    修正models.py；修改資料庫，從sqlite改為mariadb；index.html的圖片檔名從mariadb讀取

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
-    pname = models.CharField(max_length=20 ,null=False)
+class ProductModel(models.Model):
+    pname = models.CharField(max_length=30 ,null=False)
     pprice = models.IntegerField(null=False)
-    pdescription = models.TextField(max_length=255, null = False)
-    pimage = models.CharField(max_length=20, null=False)
-    pstock = models.IntegerField(null=False)
-    def __str__(self):
-        return self.pname
+    pimage = models.CharField(max_length=40, null=False)
+    pdescription = models.TextField(null = False)
+
+class OrderModel(models.Model):
+    subtotal = models.IntegerField()
+    shipping = models.IntegerField()
+    grandtotal = models.IntegerField()
+    customername = models.CharField(max_length=50)
+    customeremail = models.CharField(max_length=50)
+    customeraddress = models.CharField(max_length=50)
+    customerphone = models.CharField(max_length=50)
+
+class DetailModel(models.Model):
+    dorder = models.ForeignKey('OrderModel', on_delete=models.CASCADE)
+    pname = models.CharField(max_length=50)
+    unitprice = models.IntegerField()
+    quantity = models.IntegerField()
+    dtotal = models.IntegerField()
+    
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from myapp.form import CustomerForm
-from myapp.models import product
+from myapp.models import ProductModel, OrderModel, DetailModel
 
 # Create your views here.
 
 def index(request):
-    index =[0, 1, 2, 3, 4, 5, 6, 7] #給商品圖使用的索引值
+    products = ProductModel.objects.all()
     return render(request, 'index.html', locals())
 
 
